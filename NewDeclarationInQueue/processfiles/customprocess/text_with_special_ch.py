8041d751d7c35fb25cb3b35d5f280309a8ab53e9
Ioana Cristina Gosman <ioanacristinagosman@virtual4real.home>
2022-01-28 00:36:11 +0100

    fix processing with diacritics

--- NewDeclarationInQueue/processfiles/customprocess/text_with_special_ch.py
+++ NewDeclarationInQueue/processfiles/customprocess/text_with_special_ch.py
@@ -13,13 +13,16 @@ class TextWithSpecialCharacters:
         if not self.has_sch:
             return (self.main_string in scompare)
         
-        return (self.sch_string in scompare or \
-            self.main_string in scompare.replace('ă', 'a').replace('â', 'a').replace('î', 'i').replace('ș', 's').replace('ț', 't'))
-    
+        return (self.sch_string in scompare.encode('ascii','replace').decode("utf-8") or \
+            #self.main_string in scompare.replace('ă', 'a').replace('â', 'a').replace('î', 'i').replace('ș', 's').replace('ț', 't'))
+            self.main_string in scompare)
+        
     def startswith(self, scompare: str) -> bool:
         if not self.has_sch:
             return scompare.startswith(self.main_string)
         
-        return (scompare.startswith(self.sch_string) or \
-            scompare.replace('ä', 'a').replace('ă', 'a').replace('ã', 'a').replace('à', 'a').replace('â', 'a').replace('î', 'i').replace('ş', 's').replace('ț', 't').startswith(self.main_string))
+        
+        return (scompare.encode('ascii','replace').decode("utf-8").startswith(self.sch_string) or \
+            scompare.startswith(self.main_string))
+            #scompare.replace('ä', 'a').replace('ă', 'a').replace('ã', 'a').replace('à', 'a').replace('â', 'a').replace('î', 'i').replace('ş', 's').replace('ț', 't').startswith(self.main_string))
 
